67ba9df1b73e4b6f6397fc2ed6264f55a6d71c58
NingAn <NingAn@LAPTOP-1NRH9G1H>
2021-06-05 16:38:57 -0400

    jonathans second commit

---
+++ Greed/src/KwonMeiSinghGreedStrategy.java
@@ -0,0 +1,11 @@
+public class KwonMeiSinghGreedStrategy extends GreedStrategy{
+	public int choose(GreedOption[] options, int[] dice, int bank) {
+		return 0;
+	}
+	public String playerName() {
+		return playerName() + "'s Strategy";
+	}
+	public String author() {
+		return "Daeyong K, Jonathan M, Suraj S";
+	}
+}
